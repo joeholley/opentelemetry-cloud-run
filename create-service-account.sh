11d1619fa0f8607d0076472661d88508049c34b7
Joseph <joeholley4@gmail.com>
2024-07-08 08:44:53 +0000

    testing

--- create-service-account.sh
+++ create-service-account.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
-# Copyright 2023 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 set -ex
 
 PROJECT_ID=$(gcloud config get-value project)
 SA_NAME="run-otel-example-sa"
-REGION="us-east1"
 
 #### Create service account with required roles
 gcloud iam service-accounts create "${SA_NAME}" \
@@ -50,10 +35,4 @@ gcloud projects add-iam-policy-binding "${PROJECT_ID}" \
 gcloud projects add-iam-policy-binding "${PROJECT_ID}" \
   --member="serviceAccount:${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
   --role="roles/run.admin" \
-  --quiet
-
-#### Create artifact registry
-gcloud artifacts repositories create run-otel-example \
-  --location "${REGION}" \
-  --repository-format=docker \
   --quiet
